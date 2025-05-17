adcde683aa984af8c985243d0bbd43629b5db71a
abdellah khouden <khoudenak@gmail.com>
2025-05-18 00:57:20 +0100

    add suveillant and formateur fake login

--- backend/database/seeders/FormateurSeeder.php
+++ backend/database/seeders/FormateurSeeder.php
@@ -4,11 +4,21 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
 use App\Models\Formateur;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
 
 class FormateurSeeder extends Seeder
 {
   public function run(): void
   {
+    $user = User::factory()->create([
+      'role' => 'formateur',
+      'identifiant' => 'formateur',
+      'password' => Hash::make('formateur'),
+    ]);
+    Formateur::factory()->create([
+      'user_id' => $user->id,
+    ]);
     Formateur::factory(5)->create();
   }
 }
--- backend/database/seeders/SurveillantGeneralSeeder.php
+++ backend/database/seeders/SurveillantGeneralSeeder.php
@@ -4,11 +4,21 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
 use App\Models\SurveillantGeneral;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
 
 class SurveillantGeneralSeeder extends Seeder
 {
   public function run(): void
   {
+    $user = User::factory()->create([
+      'role' => 'surveillant',
+      'identifiant' => fake()->unique()->numerify('surveillant'),
+      'password' => Hash::make('surveillant'),
+    ]);
+    SurveillantGeneral::factory()->create([
+      'user_id' => $user->id,
+    ]);
     SurveillantGeneral::factory(5)->create();
   }
 }
